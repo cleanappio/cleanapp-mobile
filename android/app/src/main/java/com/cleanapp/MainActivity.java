c099fd064d2755a349516a8a06bf48fd191cff59
Zeeshan Ansari <zeeshanansari38244@gmail.com>
2023-09-11 15:39:54 +0500

    app name changed and publish on  gPlay 1.0.0 (1)

--- android/app/src/main/java/com/cleanapp/MainActivity.java
+++ android/app/src/main/java/com/cleanapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.dataunion.litterbux;
+package com.cleanapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Litterbux";
+    return "CleanApp";
   }
 }
--- android/app/src/main/java/com/cleanapp/MainApplication.java
+++ android/app/src/main/java/com/cleanapp/MainApplication.java
@@ -1,11 +1,10 @@
-package com.dataunion.litterbux;
+package com.cleanapp;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.lugg.RNCConfig.RNCConfigPackage;
-//import com.reactnativecommunity.cameraroll.CameraRollPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -66,7 +65,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.dataunion.cleanapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.cleanapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
