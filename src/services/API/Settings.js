23f27d29f33451e9a7ba13d7f6c06b631e736ed3
Evgeny Koshelev <eko2000@gmail.com>
2023-12-28 14:51:57 +0100

    Fix the local backend IP address

--- src/services/API/Settings.js
+++ src/services/API/Settings.js
@@ -1,7 +1,7 @@
 export const settings = {
   baseUrl: 'https://crab.dev.dataunion.app/',
   baseUrl20: 'http://34.132.121.53:80/',  // Cleanapp Google Cloud
-  baseLocalUrl: 'http://192.168.86.122:8080/',  // Cleanapp Local
+  baseLocalUrl: 'http://192.168.86.124:8080/',  // Cleanapp Local
   v2api: {
     updateOrCreateUser: 'update_or_create_user',
     updatePrivacyAndToc: 'update_privacy_and_toc',
